5250eb99bdfb19f17b12432ef34355b3ccba2c32
Mazen Ayyad <mazenayyadwork@gmail.com>
2025-01-09 15:47:53 +0300

    added function to summarize article content using BART

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from scripts.scraper import scrape
+from transformers import BartTokenizer, BartForConditionalGeneration
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+
+# loading a large model once is more efficient than for each function call
+summarizer_tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+summarizer_model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
 
 # arg is articles which is a list of dictionaries of titles and content. so far its only doing title
 def analyze_sentiment(articles):
@@ -10,10 +15,10 @@ def analyze_sentiment(articles):
 
     # analyze sentiment for each article
     for article in articles:
-        title = article["title"]
+        text_to_analyze = article["summary"]
 
         # tokenize the title for the model
-        inputs = tokenizer(title, return_tensors="pt", padding=True, truncation=True, max_length=512)
+        inputs = tokenizer(text_to_analyze, return_tensors="pt", padding=True, truncation=True, max_length=512)
 
         # send the tokenized title to the model
         outputs = model(**inputs)
@@ -61,12 +66,40 @@ def aggregate_numeric_scores(articles):
 
     return agg_label, avg_score
 
+def summarize_text(text):
+    max_tokens = 1024
+    max_summary_length = 150
+
+    # tokenize the text
+    inputs = summarizer_tokenizer.encode(text, return_tensors="pt", max_length=max_tokens, truncation=True)
+
+    summary_ids = summarizer_model.generate(inputs, max_length=max_summary_length, min_length=30, num_beams=4, early_stopping=True)
+
+    # takes the output tokens and converts them back into a string
+    summary = summarizer_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return summary
+
 if __name__ == "__main__":
     rss_url = "https://news.google.com/rss/search?q=Bitcoin&hl=en-US&gl=US&ceid=US:en"
     
     articles = scrape(rss_url)
+
+    for article in articles:
+        summary = summarize_text(article["content"])
+        article["summary"] = summary
     
     # Perform sentiment analysis on the scraped articles
     analyzed_articles = analyze_sentiment(articles)
 
+    agg_label, agg_score = aggregate_numeric_scores(analyzed_articles)
 
+    print("Aggregate Sentiment Label:", agg_label)
+    print("Aggregate Sentiment Score:", agg_score)
+    
+    for a in analyzed_articles:
+        print("---")
+        print("Title:", a["title"])
+        print("Summary:", a["summary"])
+        print("Sentiment Score:", a["sentiment_score"])
+        print("Sentiment Label:", a["sentiment_label"])
