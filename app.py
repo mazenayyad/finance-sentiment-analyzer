3b94611cf9f65f3d76887a8833dd3c4373770b92
Mazen Ayyad <mazenayyadwork@gmail.com>
2025-01-15 02:50:33 +0300

    refactored model loading to avoid spawn error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from scripts.scraper import scrape
-from scripts.analysis import summarize_text, analyze_sentiment, aggregate_numeric_scores
+from scripts.analysis import init_models, summarize_text, analyze_sentiment, aggregate_numeric_scores
 
 BITCOIN_RSS_URL = "https://news.google.com/rss/search?q=Bitcoin&hl=en-US&gl=US&ceid=US:en"
 
@@ -26,21 +26,18 @@ def results():
     """
     articles = scrape(BITCOIN_RSS_URL)
 
-    # Summarize each article
+    # summarize each article
     for article in articles:
         article["summary"] = summarize_text(article["content"])
 
-    # Perform sentiment analysis
+    # perform sentiment analysis
     analyzed_articles = analyze_sentiment(articles)
     agg_label, agg_score = aggregate_numeric_scores(analyzed_articles)
 
-    return render_template(
-        "results.html",
-        articles=analyzed_articles,
-        agg_label=agg_label,
-        agg_score=agg_score
-    )
+    return render_template("results.html", articles=analyzed_articles, agg_label=agg_label, agg_score=agg_score)
 
 
 if __name__ == "__main__":
+    # load models after importing to not clash, avoiding "spawn" errors
+    init_models() 
     app.run(debug=True)
