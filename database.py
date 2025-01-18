8e06027816085208431872f158087725d0d99854
Mazen Ayyad <mazenayyadwork@gmail.com>
2025-01-18 03:16:27 +0300

    added function insert_articles for sqlite3 db

--- database.py
+++ database.py
@@ -19,3 +19,16 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+def insert_articles(title, publish_date, summary, sentiment_score, sentiment_label):
+    conn = sqlite3.connect("articles.db")
+    c = conn.cursor()
+
+    # list columns to fill. VALUES = placeholders
+    c.execute("""
+        INSERT INTO articles (title, publish_date, summary, sentiment_score, sentiment_label)
+        VALUES (?, ?, ?, ?, ?)
+    """, (title, publish_date, summary, sentiment_score, sentiment_label))
+
+    conn.commit()
+    conn.close()
