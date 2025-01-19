d32973f87cc865fb5e182f2d8c50ba3d0fe44c9b
Mazen Ayyad <mazenayyadwork@gmail.com>
2025-01-19 22:33:45 +0300

    initialize db at the start

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from scripts.scraper import scrape
 from scripts.analysis import init_models, summarize_text, analyze_sentiment, aggregate_numeric_scores
-from database import insert_articles
+from database import insert_articles, init_db
 
 BITCOIN_RSS_URL = "https://news.google.com/rss/search?q=Bitcoin&hl=en-US&gl=US&ceid=US:en"
 
@@ -9,22 +9,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    """
-    Show a simple landing page with a link to /results.
-    """
     return render_template("index.html")
 
 
 @app.route("/results")
 def results():
-    """
-    When the user hits /results, we'll:
-    1) Scrape articles
-    2) Summarize them
-    3) Analyze sentiment
-    4) Aggregate scores
-    5) Render everything in results.html
-    """
     articles = scrape(BITCOIN_RSS_URL)
 
     # summarize each article
@@ -51,6 +40,7 @@ def results():
 
 
 if __name__ == "__main__":
+    init_db()
     # load models after importing to not clash, avoiding "spawn" errors
     init_models() 
     app.run(debug=True)
