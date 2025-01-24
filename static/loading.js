c43b5a23b4d90c202c7a6a96ee7a4f18f6096fe9
Mazen Ayyad <mazenayyadwork@gmail.com>
2025-01-24 23:35:46 +0300

    refactor. went back to inline js

--- static/loading.js
+++
@@ -1,9 +0,0 @@
-setInterval(function() {
-    fetch("/check_status")
-      .then(resp => resp.text())
-      .then(responseText => {
-        if (responseText === "done") {
-          window.location.href = "/results";
-        }
-      });
-  }, 2000);  
